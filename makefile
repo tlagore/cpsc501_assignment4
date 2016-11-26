d79804b12105e14be0cdc9b37cd25c82fce27aa5
tyrone.lagore <tyrone.lagore@csx3.cs.ucalgary.ca>
2016-11-26 14:55:50 -0700

    created wav_header.h which holds the definition for a struct that defines the format of a .wav file.

--- makefile
+++ makefile
@@ -1,8 +1,9 @@
 CC=gcc
 TARGET=time_d_conv
 
-all: time_d_conv.c
+all: time_d_conv.c wav_header.h
 	$(CC) -o $(TARGET) $(TARGET).c
 
 clean:
 	$(RM) $(TARGET)
+
--- time_d_conv.c
+++ time_d_conv.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+//User defined header files
+#include "wav_header.h"
+
 int main(int argc, char*argv[])
 {
-  printf("hello");
+  struct wav_header w_header;
 
+  w_header.file_size = 200;
+  printf("%d", w_header.file_size);
+
+  return 0;
 }
